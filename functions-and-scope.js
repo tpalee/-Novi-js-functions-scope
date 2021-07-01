a81fc8a0a27a2066c6723d267ac7fea287fe27eb
tpalee <thieulee@hotmail.com>
2021-07-01 22:10:22 +0200

    exercises

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -13,6 +13,13 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // * Hoe zorg ik ervoor dat dit ook werkt wanneer de array 100 entries bevat?
 // * Hoe zorgt ik ervoor dat wanneer ik een cijfer tegenkom die aan de conditie voldoet, ik dit ergens kan bijhouden?
 // Log het antwoord in de terminal.
+let eightOrHigher = [];
+for (let i = 0; i < grades.length; i++) {
+    if (grades[i] >= 8) {
+        eightOrHigher.push(grades[i]);
+    }
+}
+console.log(eightOrHigher.length);
 
 // ---- Verwachte uitkomst: 6
 
@@ -22,12 +29,29 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // Zorg ervoor dat jouw functie ook werkt als we een andere array met eindcijfers willen checken, zoals bijvoorbeeld: [6, 4, 5] of [8, 9, 4, 6, 10].
 // Log het antwoord in de terminal.
 
+function cumLaude(array) {
+    const cumLaudeArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] >= 8) {
+            cumLaudeArray.push(array[i]);
+        }
+    }
+    return cumLaudeArray.length;
+}
+
+const gradesArray1 = [6, 4, 5];
+const gradesArray2 = [8, 9, 4, 6, 10];
+
+const cumLaudeStudents1 = cumLaude(gradesArray1);
+const cumLaudeStudents2 = cumLaude(gradesArray2);
+
+console.log(cumLaudeStudents1, cumLaudeStudents2);
+
+
 // ---- Verwachte uitkomsten:
 // cumLaude(grades) geeft 6
 // cumLaude([6, 4, 5]) geeft 0
 // cumLaude([8, 9, 4, 6, 10]) geeft 3
-
-
 
 
 /* Opdracht  2: Gemiddeld cijfer */
@@ -39,14 +63,36 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // * Wat moet ik verzamelen uit de array van cijfers om uiteindelijk een gemiddelde te kunnen berekenen?
 // * Hoe zorgt ik ervoor dat ik alle waardes uit de array kan langslopen, ook als de array wel 100 entries zou bevatten?
 // Log het antwoord in de terminal.
+let total = 0;
+for (let i = 0; i < grades.length; i++) {
+    total += grades[i];
+}
+const average = total / grades.length;
+console.log(average);
+
 
 // ---- Verwachte uitkomst: 6.642857142857143
 
 
-/* 2b: Omschrijven tot een herbruikbare functie */
+/* 2b: Omschrijven tot een herbruikbare functies */
 // Schrijf een functie genaamd averageGrade, die een array van cijfers verwacht (zoals grades) en het gemiddelde cijfer teruggeeft. Gebruik hiervoor jouw antwoord van 2a.
 // Zorg ervoor dat jouw functie ook werkt als we een andere array willen checken, zoals bijvoorbeeld: [6, 4, 5] of [8, 9, 4, 6, 10].
 // Log het antwoord in de terminal.
+
+function averageGrade(array) {
+    let totalGrades = 0;
+    for (let i = 0; i < array.length; i++) {
+        totalGrades += array[i];
+    }
+    totalGrades = totalGrades / array.length;
+    return totalGrades.toFixed(2)
+}
+
+let averageGrade1 = averageGrade(grades);
+let averageGrade2 = averageGrade([6, 4, 5]);
+let averageGrade3 = averageGrade([8, 9, 4, 6, 10]);
+
+console.log(averageGrade1, averageGrade2, averageGrade3);
 
 // ---- Verwachte uitkomsten:
 // averageGrade(grades) geeft 6.642857142857143
@@ -59,8 +105,6 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // Tip: Google is your best friend!
 
 
-
-
 /* Bonusopdracht: hoogste cijfer */
 
 /* 3a: Script schrijven  */
@@ -71,12 +115,36 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // Log het antwoord in de terminal.
 
 // ---- Verwachte uitkomst: 9
-
-
+let highNum = grades[0];
+for (let i = 0; i < grades.length - 1; i++) {
+    let compareNum = grades[i + 1];
+    if (compareNum > highNum) {
+        highNum = compareNum;
+    }
+}
+console.log(highNum);
 /* 3b: Omschrijven tot een herbruikbare functie */
 // Schrijf een functie genaamd highestGrade, die een array van cijfers verwacht (zoals grades) en het hoogste cijfer teruggeeft. Gebruik hiervoor jouw antwoord van 3a.
 // Zorg ervoor dat jouw functie ook werkt als we een andere array willen checken, zoals bijvoorbeeld: [6, 4, 5] of [8, 9, 4, 6, 10].
 // Log het antwoord in de terminal.
+
+
+function highestGrade(array) {
+    let highNum = array[0];
+    for (let i = 0; i < array.length - 1; i++) {
+        let compareNum = array[i + 1];
+        if (compareNum > highNum) {
+            highNum = compareNum;
+        }
+    }
+    console.log(highNum);
+}
+
+// check if function works with other arrays
+const highestNumber1 = highestGrade(grades);
+const highestNumber2 = highestGrade([6, 4, 5]);
+const highestNumber3 = highestGrade([8, 9, 4, 6, 10]);
+
 
 // ---- Verwachte uitkomsten:
 // highestGrade(grades) geeft 9
--- functions-methods.js
+++ functions-methods.js
@@ -9,6 +9,17 @@
 // getEmailDomain("t.mellink@novi.nl") geeft novi.nl
 // getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
 
+function getEmailDomain(email){
+    const mailsplit=email.split('@');
+    const result=mailsplit[1];
+    return result;
+}
+
+// return emaildomain
+const emailDomain1=getEmailDomain("n.eeken@novi-education.nl");
+const emailDomain2=getEmailDomain("t.mellink@novi.nl");
+const emailDomain3=getEmailDomain("a.wiersma@outlook.com");
+console.log(emailDomain1, emailDomain2,emailDomain3);
 
 
 
@@ -20,6 +31,24 @@
 // typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
 
+function typeOfEmail(email) {
+    let result = "";
+    if (email.includes('@novi-education.nl')) {
+        result = "student";
+    } else if (email.includes('@novi.nl')) {
+        result = "Medewerker";
+    } else {
+        result = "Extern"
+    }
+    return result;
+}
+
+//check types of Emails
+const emailCheck1=typeOfEmail("n.eeken@novi-education.nl");
+const emailCheck2=typeOfEmail("t.mellink@novi.nl");
+const emailCheck3=typeOfEmail("novi.nlaapjesk@outlook.com");
+const emailCheck4=typeOfEmail("a.wiersma@outlook.com");
+console.log(emailCheck1,emailCheck2,emailCheck3,emailCheck4);
 
 
 /* Opdracht  3 */
@@ -34,3 +63,22 @@
 // checkEmailValidity("n.eekenanovi.nl") geeft false - want geen @
 // checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
 // checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
+
+function checkEmailValidity(email) {
+    if ((email.charAt(email.length - 1) == '.') || (email.includes(','))) {
+        return false;
+    } else if (!email.includes('@')) {
+        return false;
+    } else {
+        return true
+    }
+}
+
+//check if mailadresses are valid
+const validMailAdress1 = checkEmailValidity('n.eeken@novi.nl');
+const validMailAdress2 = checkEmailValidity('tessmellink@novi.nl');
+const validMailAdress3 = checkEmailValidity('n.eekenanovi.nl');
+const validMailAdress4 = checkEmailValidity('n.eeken@novinl.');
+const validMailAdress5 = checkEmailValidity('tessmellink@novi,nl');
+
+console.log(validMailAdress1, validMailAdress2, validMailAdress3, validMailAdress4, validMailAdress5);
